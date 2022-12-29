ff643ec4b9ddaecc37d13c90d25b67e6626c8db6
kylian <kylian.huerta.alfaro@hva.nl>
2022-12-29 18:24:40 +0100

    Add project files.

---
+++ poptabs/Pages/Test.razor.js
@@ -0,0 +1,14 @@
+﻿const mainChannel = new BroadcastChannel("channel");
+
+mainChannel.onmessage = (e) => {
+  alert(e.data);
+  if (dotNetHelper) {
+    dotNetHelper.invokeMethodAsync('BlazorSample', 'InvokeAction');
+  }
+}
+
+var dotNetHelper = null;
+
+window.updateMessageCaller = (dotNetHelper) => {
+  dotNetHelper = dotNetHelper;
+}
---
+++ poptabs/wwwroot/js/pop.js
@@ -0,0 +1,20 @@
+﻿const mainChannel = new BroadcastChannel("channel");
+
+mainChannel.onmessage = (e) => {
+  alert(e.data);
+  dotNetHelperCache.forEach(helper =>
+    helper.invokeMethodAsync('BlazorSample', 'InvokeAction')
+  );
+}
+
+var dotNetHelperCache = [];
+
+window.SendStateOfLastButtonClicked = () => {
+  tempChannel = new BroadcastChannel("channel");
+  tempChannel.postMessage("test message");
+  tempChannel.close();
+}
+
+window.updateMessageCaller = (dotNetHelper) => {
+  dotNetHelperCache.push(dotNetHelper);
+}
